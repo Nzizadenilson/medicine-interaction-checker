b62378949123e0d6ec2c48726783f3b4749e929f
Nzizadenilson <g.nziza@alustudent.com>
2026-07-27 23:31:14 +0200

    Implement medication search using HTTPServer and JavaScript

---
+++ medication/medication_checker/app.py
@@ -0,0 +1,74 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
+import json
+import requests
+
+class MedicationHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        #serving the index.html
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            FILE = open("index.html", "r")
+            self.wfile.write(FILE.read().encode())
+        #serving the styles.css
+        elif self.path == '/styles.css':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/css')
+            self.end_headers()
+            FILE = open("styles.css", "r")
+            self.wfile.write(FILE.read().encode())
+        #serving the script.js
+        elif self.path == '/medication_js/script.js':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/javascript')
+            self.end_headers()
+            FILE = open("medication_js/script.js", "r")
+            self.wfile.write(FILE.read().encode())
+        #search for the medicine
+        elif self.path.startswith('/medicine'):
+            query = urllib.parse.urlparse(self.path).query
+            parameters = urllib.parse.parse_qs(query)
+            medicine = parameters.get('medicine', [None])[0]
+            if medicine:
+                url = (
+                    "https://api.fda.gov/drug/label.json"
+                    f"?search=openfda.generic_name:{medicine}"
+                    "&limit=1"
+                )
+                response = requests.get(url)
+                data = response.json()
+                #Error handling if the medicine is not found
+                try:
+                    drug = data['results'][0]
+                    medicine_info = {
+                        "name": drug.get("openfda", {}).get("brand_name", ["N/A"])[0],
+                        "description": drug.get("description", ["N/A"])[0],
+                        "warnings": drug.get("warnings", ["No warnings found"])[0],
+                        "dosage": drug.get("dosage_and_administration", ["No dosage information found"])[0],
+                        "instructions": drug.get("instructions_for_use", ["No instructions available"])[0]
+                    }
+                except (KeyError, IndexError):
+                    medicine_info = {"error": "Medicine not found or API limit reached."}
+
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(medicine_info).encode())
+            else:
+                #If no medicinie name is provided
+                self.send_response(400)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                error_message = {"error": "No medicine name provided."}
+                self.wfile.write(json.dumps(error_message).encode())
+        else:
+            #wrong url path
+            self.send_response(404)
+            self.end_headers()
+server = HTTPServer(('localhost', 8000), MedicationHandler)
+print("Server running on http://localhost:8000")
+server.serve_forever()
+
---
+++ medication/medication_checker/medication_js/script.js
@@ -0,0 +1,51 @@
+const medicine_name = document.getElementById("medicine");
+const form = document.getElementById("medicationform");
+const results = document.getElementById("results");
+const button = document.getElementById("button");
+
+function checkMedicine() {
+    if (medicine_name.value.trim() === "") {
+        alert("Please enter a medicine name.");
+        return false;
+    }
+    return true;
+}
+
+form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    if (!checkMedicine()) {
+        return;
+    }
+    button.disabled = true;
+    button.textContent = "Searching...";
+
+    try {
+        const response = await fetch(`/medicine?medicine=${encodeURIComponent(medicine_name.value)}`);
+        const data = await response.json();
+        if (data.error) {
+            results.innerHTML = `<p>${data.error}</p>`;
+        }
+        else {
+            results.innerHTML = `
+            <div class="medicine-card">
+                <h2>${data.name}</h2>
+                <h3>Description</h3>
+                    <p>${data.description}</p>
+                 <h3>Warnings</h3>
+                    <p>${data.warnings}</p>
+                <h3>Dosage</h3>
+                    <p>${data.dosage}</p>
+                <h3>Instructions</h3>
+                    <p>${data.instructions}</p>
+            </div>
+            `;
+        }
+    }
+    catch (error) {
+        results.innerHTML = `<p>Unable to retrieve medicine information.</p>`;
+
+    }
+    button.disabled = false;
+    button.textContent = "Search";
+});
